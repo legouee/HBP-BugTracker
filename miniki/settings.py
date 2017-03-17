e66aacaf8f835670dba6a90cc42a8f8408582c1a
Joffrey Gonin <Joffrey.Gonin@gmail.com>
2017-03-17 11:08:15 +0100

    database outside

--- miniki/settings.py
+++ miniki/settings.py
@@ -15,7 +15,18 @@ import os
 from miniki.config import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+
+
+# Database
+# https://docs.djangoproject.com/en/1.10/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 
 # Quick-start development settings - unsuitable for production
@@ -110,15 +121,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'miniki.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 
 # Password validation
