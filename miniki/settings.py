ebe482530e05dda5678bae7600a4bd87f08c2046
Joffrey Gonin <Joffrey.Gonin@gmail.com>
2017-04-14 10:52:25 +0200

    again

--- miniki/settings.py
+++ miniki/settings.py
@@ -120,9 +120,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'miniki.wsgi.application'
 
-CTX = ''
-
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
