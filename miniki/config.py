1c8b4bf0e5d33dca8144b5dca482dcac68bd88db
HFragnaud <helissande.fragnaud@gmail.com>
2017-04-18 16:17:09 +0200

    c

--- miniki/config.py
+++ miniki/config.py
@@ -1,3 +1,7 @@
 import hbp_app_python_auth.settings as auth_settings
-auth_settings.SOCIAL_AUTH_HBP_KEY = 'f0f138fb-6fa9-4e85-a20c-fddcec97a4cf'
-auth_settings.SOCIAL_AUTH_HBP_SECRET = 'RIsmRMIVFyyzCKHcuzEJLyq21yoHx4anzJa9jmc0Thse4vIhYgGP5USChpglLW0zWBbPdLbXd3KfPEwN1hqPog'
+#auth_settings.SOCIAL_AUTH_HBP_KEY = 'f0f138fb-6fa9-4e85-a20c-fddcec97a4cf'
+#auth_settings.SOCIAL_AUTH_HBP_SECRET = 'RIsmRMIVFyyzCKHcuzEJLyq21yoHx4anzJa9jmc0Thse4vIhYgGP5USChpglLW0zWBbPdLbXd3KfPEwN1hqPog'
+
+#heli
+auth_settings.SOCIAL_AUTH_HBP_KEY = '7a0f11f2-a4a4-49b7-8547-92f0e9a765db'
+auth_settings.SOCIAL_AUTH_HBP_SECRET = 'VeGcoR09nsgSqgZq-Tux1cVMmWHvb9Ot7AQTPAtd7X4yML_8xrS7-BFCpbhRN2TGKgz7nquJw-nx0fasfND6MA'
